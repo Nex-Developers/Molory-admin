d0dd79328713b0207e98e305f83a3618740c36c4
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-08-15 22:54:15 +0000

    Redesign login

--- pages/login.vue
+++ pages/login.vue
@@ -1,97 +1,165 @@
 <script setup lang="ts">
 import { ref } from "@vue/reactivity";
-import {loginWithEmail} from "~/composables/useAuth";
+import { loginWithEmail } from "~/composables/useAuth";
 
-const email = ref(null)
-const password = ref(null)
-const hasError = ref(null)
-const errorMessage = ref(null)
-
+const email = ref(null);
+const password = ref(null);
+const hasError = ref(null);
+const errorMessage = ref(null);
 
 definePageMeta({
   // middleware: 'guest'
-})
+});
 
 const postLoginForm = async function () {
-  await loginWithEmail(email.value, password.value)
-}
+  await loginWithEmail(email.value, password.value);
+};
 </script>
 
 <template>
-  <div class="h-screen bg-gradient-to-b from-white to-blue-200 dark:bg-gradient-to-b dark:from-slate-800 dark:to-slate-400">
-    <div class="flex items-center justify-center  px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
-        <div>
-          <div class="pt-5 h-25 w-25">
+<div class="w-full h-screen px-12 flex justify-center bg-gray-200 items-center">
+  
+  <div class="w-10/12 xl:w-8/12">
+    <div class="block bg-white shadow-lg rounded-lg">
+      <div class="lg:flex lg:flex-wrap g-0">
+        <div class="lg:w-6/12 px-4 md:px-0">
+          <div class="md:p-12 md:mx-6">
+            <div class="text-center">
+              <img
+                class="mx-auto w-48"
+                src="/img/logo.png"
+                alt="logo"
+              />
+              <h4 class="text-xl font-semibold mt-1 mb-12 pb-1">
+                <!-- We are The Lotus Team -->
+              </h4>
+            </div>
+            <form>
+              <p class="mb-4">Please login to your account</p>
+              <div class="mb-4">
+                <input
+                  v-model="email"
+                  type="email"
+                  class="
+                    form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700
+                    focus:bg-white
+                    focus:border-blue-600
+                    focus:outline-none
+                  "
+                  id="exampleFormControlInput1"
+                  placeholder="Email address"
+                  autocomplete="email"
+                  required
+                />
+              </div>
+              <div class="mb-4">
+                <input
+                  type="password"
+                  v-model="password"
+                  class="
+                    form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700
+                    focus:bg-white
+                    focus:border-blue-600
+                    focus:outline-none
+                  "
+                  id="exampleFormControlInput1"
+                  placeholder="Password"
+                  autocomplete="current-password"
+                  required
+                />
+              </div>
+              <div class="text-center pt-1 mb-12 pb-1">
+                <button
+                  @click.prevent="postLoginForm"
+                    style="background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);"
+                  class="
+                    inline-block
+                    px-6
+                    py-2.5
+                    text-white
+                    font-medium
+                    text-xs
+                    leading-tight
+                    uppercase
+                    rounded
+                    shadow-md
+                    hover:bg-blue-700 hover:shadow-lg
+                    focus:shadow-lg focus:outline-none focus:ring-0
+                    active:shadow-lg
+                    transition
+                    duration-150
+                    ease-in-out
+                    w-full
+                    mb-3
+                  "
+                  type="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                  
+                >
+                  Log in
+                </button>
+                <a class="text-gray-500" href="#!">Forgot password?</a>
+              </div>
+            
+            </form>
           </div>
-          <div class="flex">
-          <img class="mx-auto h-36 w-auto" src="/img/logo.png" alt="molory logo" />
-          <!-- <h1 class="py-9 text-center text-5xl font-extrabold text-gray-900 dark:text-gray-400">Molory </h1> -->
-          </div>
-          
-          <h2 class="mt-6 py-9 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-400">Sign in</h2>
         </div>
-
-        <div v-if="hasError" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert">
-          <strong class="font-bold">Oops, try again! </strong>
-          <span class="block sm:inline">{{ errorMessage }}</span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg class="fill-current h-6 w-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <title>Close</title>
-              <path
-                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
+        <div
+        style=" background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);"
+          class="
+            lg:w-6/12
+            flex
+            items-center
+            lg:rounded-r-lg
+            rounded-b-lg
+            lg:rounded-bl-none
+          "
+        >
+          <div class="text-white px-4 py-6 md:p-12 md:mx-6">
+            <h4 class="text-xl font-semibold mb-6">
+              We are more than just a company
+            </h4>
+            <p class="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
         </div>
-
-        <form v-if="true" v-on:submit.prevent class="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" value="true">
-          <div class="rounded-md shadow-sm -space-y-px mb-1">
-            <div>
-              <label for="email-address" class="sr-only">Email address</label>
-              <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required
-                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address">
-            </div>
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password"
-              required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password">
-          </div>
-
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <input id="remember-me" name="remember-me" type="checkbox"
-                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-              <label for="remember-me" class="ml-2 block text-sm text-gray-900 dark:text-white"> Remember me </label>
-            </div>
-
-            <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 dark:text-white"> Forgot your password? </a>
-            </div>
-          </div>
-
-          <div>
-            <button @click.prevent="postLoginForm"
-              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-secondary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                <!-- Heroicon name: solid/lock-closed -->
-                <!-- <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clip-rule="evenodd" />
-                </svg> -->
-              </span>
-              Sign in
-            </button>
-          </div>
-        </form>
       </div>
     </div>
   </div>
+      
+</div>
+ 
 </template>
