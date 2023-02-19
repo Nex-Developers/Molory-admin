322e621dc7c8fdea5e3fac28770d7e6a7e1dc1ba
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-02-19 23:05:49 +0000

    fix prod mod issues

--- app.vue
+++ app.vue
@@ -1,23 +1,23 @@
 <template>
-    <NuxtPage />
+  <NuxtPage />
 </template>
 <script setup lang="ts">
 import { useUser } from "~/composables/useAuth";
 useHead({
-  title: "Molory Admin",
-  // or, instead:
-  // titleTemplate: (title) => `My App - ${title}`,
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-  // charset: "utf-8",
-  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
-  meta: [{ name: "description", content: "My amazing site." }],
-  bodyAttrs: {
-    class: "test",
-  },
+title: "Molory Admin",
+// or, instead:
+// titleTemplate: (title) => `My App - ${title}`,
+// viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+// charset: "utf-8",
+link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+meta: [{ name: "description", content: "My amazing site." }],
+bodyAttrs: {
+  class: "test",
+},
 });
 const nuxtApp = useNuxtApp();
 nuxtApp.hook("page:finish", () => {
-  window.scrollTo(0, 0);
+window.scrollTo(0, 0);
 });
 await useUser();
 </script>
